b987d5e2ae70547fff6cfc75148b8100324dc203
tonah <onahpatience1610@gmail.com>
2026-06-09 14:59:27 +0100

    modified the main.go file

--- solution/Task-two/main.go
+++ solution/Task-two/main.go
@@ -1,1 +1,16 @@
 package main
+
+import "fmt"
+
+func Q2IsEven(n int) bool {
+	num  := n % 2
+	if num == 0 {
+		return true
+	} else {
+		return false
+	}
+}
+
+func main() {
+	fmt.Println(Q2IsEven(2))
+}
